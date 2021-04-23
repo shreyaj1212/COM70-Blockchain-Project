243847d098513c5ea4c6df2f1878f88fb0c2a055
Grace <gkluettgen@mxschool.edu>
2021-04-22 21:47:01 -0400

    Blockchain

--- components/Blockchain.js
+++ components/Blockchain.js
@@ -1,9 +1,24 @@
 Node = require('./Node');
 
 class Blockchain {
-    constructor(startNode) {
-        this.firstNode = startNode;
+    constructor(owner) {
+        this.owner = owner;
         this.chain = [];
+    }
+
+    lastBlock()
+    {
+        return chain[chain.length-1];
+    }
+
+    addNewBlock(Block)
+    {
+        /* hash = hash_funciton(proofOfWork) */
+        /*if(Block.getHash == function(lastBlock().getProofOfWork())
+        && function(Block.getProofOfWork()).substr(0,4) == "0000" )
+        {
+            chain.push(Block);
+        }*/
     }
 }
 
