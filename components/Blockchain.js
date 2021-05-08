3e1b60497cbf013bfe0153c1b38ff58c45713dda
Grace <gkluettgen@mxschool.edu>
2021-05-08 16:37:10 -0400

    blockchain

--- components/Blockchain.js
+++ components/Blockchain.js
@@ -1,18 +1,26 @@
-Node = require('./User');
+User = require('./User');
 
 class Blockchain {
-    constructor() {
-        // this.firstNode = startNode;
+    constructor(owner) {
+        this.owner = owner;
         this.chain = [];
     }
 
-    addBlockToChain(block) {
-        this.chain.push(block);
+    lastBlock()
+    {
+        return chain[chain.length-1];
     }
 
-    getLastBlock() {
-
+    addNewBlock(Block)
+    {
+        if((Block.getPrecedingHash() === null||Block.getPrecedingHash() == lastBlock().getHash())
+        && createHmac('sha256', Block.getProofOfWork()).digest('hex').substr(0,1) == "0000" )
+        {
+            chain.push(Block);
+        }
     }
 }
 
 module.exports = Blockchain;
+
+
