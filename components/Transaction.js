e39ab1c0afae39e329c9031af944ba31ff9ead01
Grace <gkluettgen@mxschool.edu>
2021-05-15 13:48:36 -0400

    transaction

--- components/Transaction.js
+++ components/Transaction.js
@@ -1,6 +1,4 @@
-const alphabet = ['a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u',
-'v','w','x','y','z'];
-
+const uuid = require('uuid')
 class Transaction {
 
     constructor(buyNode, sellNode, amount, sign) {
@@ -52,17 +50,7 @@ class Transaction {
 
     randomString()
     {
-        while(true)
-        {
-            var s = "";
-            for(var i = 0; i<5; i++)
-            {
-                s = s+alphabet[Math.floor(Math.random()*26)];
-            }
-            /*if unique*/
-            break;
-        }
-        return s;
+        return uuid.v4();
     }
 
 }
